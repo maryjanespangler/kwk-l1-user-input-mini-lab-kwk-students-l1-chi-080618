0edf635801f2a3ebb4490dd923aaf42f6a52645d
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-09 16:08:23 +0000

    automatically backed up by learn

--- 1d.rb
+++ 1d.rb
@@ -2,13 +2,12 @@ one_direction = {"Zayn" => 25,
                   "Liam" => 24,
                   "Harry" => 24, 
                   "Nial" => 24
-  
-}
+  }
                   
                 
                  one_direction.each do |name, age|
                    
-                end
+               
                   puts "#{name}"
                   puts "#{age}"
-                  
+                 end
