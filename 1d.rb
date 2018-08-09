731721fb7a36d8b5ed93c9015ba0af492933e642
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-09 16:01:52 +0000

    automatically backed up by learn

--- 1d.rb
+++ 1d.rb
@@ -1,8 +1,9 @@
 one_direction = {"Zayn" => 25,
                   "Liam" => 24,
                   "Harry" => 24, 
-                  "Nial" => 24}
+                  "Nial" => 24
+  
+}
                   
                   def names_and_age
-                    puts one_direction.each do 
-                      
+                    puts one_direction.each do |name,age|
