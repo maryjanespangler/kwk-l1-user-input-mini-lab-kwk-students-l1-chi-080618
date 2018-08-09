e9858d3f6f3040cd2f540ecdefa74e343fbd606a
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-09 15:55:26 +0000

    automatically backed up by learn

---
+++ 1d.rb
@@ -0,0 +1,7 @@
+one direction_ = {"Zayn" => 25,
+                  "Liam" => 24,
+                  "Harry" => 24, 
+                  "Nial" => 24}
+                  
+                  def names_and_age
+                    
