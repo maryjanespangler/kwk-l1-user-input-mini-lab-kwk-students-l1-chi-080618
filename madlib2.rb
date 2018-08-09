d734774eb3481653f45e135ae8c5f8ecc166c6ef
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-09 15:25:12 +0000

    automatically backed up by learn

---
+++ madlib2.rb
@@ -0,0 +1,23 @@
+def option_a
+  puts "give me a name of a person"
+  person1 - gets.chomp
+  puts "give me a verb ending in ing "
+  verbing1 - gets.chomp
+  puts "give me an adjective"
+  adjective1 - gets.chomp
+  puts "give me a wild animal"
+  animal1 - gets.chomp
+  puts "give me a red food"
+  red1 gets.chomp
+  puts "give me a holiday"
+  holiday1 - gets.chomp
+  puts "give me a place"
+  place1 - gets.chomp
+  puts "give me an occupation"
+  occupation1 - gets.chomp
+  
+   puts "HI! My name is #{person1} and i'm going to tell you a little story. One day I was at home just #{verbing1} and eating some #{adjective1} ice cream and the suddenly the tv showed an interview about an adoption center for #{animal1} babbies! My face immedietly turns #{red1} red cause I need one!!! But wait, my parents aren't going to just buy me one. Hmmm maybe if I ask for one for #{holiday1}. But just to make sure, I need to make bank incase they won't buy it for me. I think i'm going to apply for a job at #{place1} as a #{occupation}. Well, that's my story i'll let you know if they accept me!!!"
+ end
+ 
+ option_a  
+  
